bc32d9bd59566a79e521427550f6778cda833e04
Kenny Egun <kennyegun240@gmail.com>
2023-06-28 22:03:50 +0100

    Add comments to the code

--- assignment2.py
+++ assignment2.py
@@ -1,7 +1,11 @@
+# function to calculate the circumference and it takes one parameter
 def print_circum(radius):
+    # the circumference variable is assigned the final result after the basic operatioin has been done
     circumference=2 * 3.14159 * radius
+    # This then print out the final result
     print(f"The circumference of the radius is {circumference}")
 
+# Calling the function and passing different parameters
 print_circum(300)
 print_circum(140)
 print_circum(20)
--- assignment3.py
+++ assignment3.py
@@ -1,20 +1,27 @@
-
+# initialized list of items and gave them values as their prices
 item_prices = {
     'ITEM1': 134,
     'ITEM2': 5321,
     'ITEM3': 32313
 }
 
-
+# Function to print the individual items. I decided to make this a global function so that it
+# will get separated from the calculation logic and also to make code cleaner
+# It takes the order prducts and price as parameters and prints it out
 def print_products_price(product, price):
     print(f"{product:<30}", end=f"{str(price)}\n")
 
+# Function to print the combined items bought by the customer and also to display
+# the total price.
+# It takes the order prducts and price as parameters and prints it out
 def print_combos(product, price):
     print(f"Combo{product:<25}", end=f"{str(price)}\n")
 
+# Function to print the contact
 def print_contact():
     print('For delivey contact us on: +234 805 254 64789')
 
+# Function to calculate each order. It takes a parameter which is an array
 def calculate_purchase(params):
     check_num_of_item = len(params)
     total = 0
@@ -26,24 +33,33 @@ def calculate_purchase(params):
     print_products_price('ITEM2',item_prices['ITEM2'])
     print_products_price('ITEM3',item_prices['ITEM3'])
 
+# This conditional statement is used to cheeck the length of the parameter passed
+# to the function so different operations will be carried out if the conditions are met.
     if check_num_of_item == 1:
         total = item_prices[params[0]]
+
+        # Here i passed the parameter to the print_combos and also the total, so it prints out the
         print_combos(f"({params[0]})", total)
 
+    # This is an else if statement checking if the length of thr parameter is 2, if it is 2, it will then carry out the codes in the block
     elif check_num_of_item == 2:
+        # This is checking the parameters being sent to the function
         if ('ITEM1' in params and 'ITEM2' in params) or ('ITEM1' in params and 'ITEM3' in params) or ('ITEM2' in params and 'ITEM3' in params):
             sum1 = item_prices[params[0]]
             sum2 = item_prices[params[1]]
             total = sum1 + sum2 * 0.9
+            # The parameters and also the total values are being passed to the print_combos function
             print_combos(f"({params[0]} + {params[1]})", total)
 
+    # This is an else if statement checking if the length of thr parameter is 3, if it is 2, it will then carry out the codes in the block
     elif check_num_of_item == 3:
         total = item_prices[params[0]] + item_prices[params[1]] + item_prices[params[2]] * 0.75
+            # The parameters and also the total values are being passed to the print_combos function
         print_combos(f"({params[0]} + {params[1]} + {params[2]})", total)
 
     print('----------------------------------')
+    # This is printing the contact after the calculations has been completed
     print_contact()
-    return total
 
 item1 = ['ITEM2']
 calc1_price = calculate_purchase(item1)
